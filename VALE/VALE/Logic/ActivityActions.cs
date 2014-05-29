bdf28a838f1a89c17305d33a01e0431b272b21e2
Domenico Moreschini <domenico.moreschini@gmail.com>
2014-05-29 16:11:04 +0200

    Aggiunta class ActivityActions

---
+++ VALE/VALE/Logic/ActivityActions.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using VALE.Models;
+
+namespace VALE.Logic
+{
+    public class ActivityActions : IDisposable
+    {
+        UserOperationsContext _db = new UserOperationsContext();
+
+        public List<Activity> GetConcludedActivities(string userId)
+        {
+            var activitiesId = _db.Reports.Where(r => r.WorkerId == userId).GroupBy(r => r.ActivityId).Select(gr => gr.Key).ToList();
+            return _db.Activities.Where(a => activitiesId.Contains(a.ActivityId)).ToList();
+        }
+
+        public string GetHoursWorked(string userId, int activityId)
+        {
+            return _db.Reports.Where(r => r.ActivityId == activityId && r.WorkerId == userId).Sum(r => r.HoursWorked).ToString();
+        }
+
+        public void Dispose()
+        {
+            if (_db != null)
+                _db = null;
+        }
+    }
+}
