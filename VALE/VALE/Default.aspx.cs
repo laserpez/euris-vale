10c59711857257ff4a236b2de1605abeb617fabb
Mekni Oussama <ing.oussama.mekni@gmail.com>
2014-06-11 16:43:17 +0200

    parse su data

---
+++ VALE/VALE/Default.aspx.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using VALE.Models;
+
+namespace VALE
+{
+    public partial class _Default : Page
+    {
+        private UserData _currentUser;
+        private UserOperationsContext _db;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            _db = new UserOperationsContext();
+            notLoggedUser.Visible = !HttpContext.Current.User.Identity.IsAuthenticated;
+            loggedUser.Visible = HttpContext.Current.User.Identity.IsAuthenticated;
+            _currentUser = _db.UsersData.FirstOrDefault(u => u.UserName == User.Identity.Name);
+        }
+
+        protected void btnViewAll_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        public IQueryable<Project> GetProjects()
+        {
+            if (_currentUser != null)
+                return _currentUser.AttendingProjects.Take(10).AsQueryable();
+            else
+                return null;
+        }
+
+        public IQueryable<Event> GetEvents()
+        {
+            return null;
+        }
+
+        public IQueryable<Activity> GetActivities()
+        {
+            return null;
+        }
+
+        protected void btnViewProjectDetails_Click(object sender, EventArgs e)
+        {
+
+        }
+        
+    }
+}
