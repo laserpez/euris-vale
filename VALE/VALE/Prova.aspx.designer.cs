0a97bac45093d6c6d653c31fdafa4334a00aa8a2
Mekni Oussama <ing.oussama.mekni@gmail.com>
2014-06-12 17:08:49 +0200

    commit

--- VALE/VALE/Prova.aspx.designer.cs
+++ VALE/VALE/Prova.aspx.designer.cs
@@ -40,6 +40,15 @@ namespace VALE {
         protected global::AjaxControlToolkit.ModalPopupExtender ModalPopup;
         
         /// <summary>
+        /// TextBox1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextBox1;
+        
+        /// <summary>
         /// TitleMpdalView control.
         /// </summary>
         /// <remarks>
