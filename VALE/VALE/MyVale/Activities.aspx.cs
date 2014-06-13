77381674c4fd98938bbb2b147d46ace8b89736c9
Domenico Moreschini <domenico.moreschini@gmail.com>
2014-06-13 16:53:47 +0200

    Inizio modifiche Activity

---
+++ VALE/VALE/MyVale/Activities.aspx.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Microsoft.AspNet.Identity;
+using VALE.Models;
+using CsvHelper;
+using System.IO;
+using System.Text;
+using VALE.Logic;
+
+namespace VALE.MyVale
+{
+    public partial class Activities : System.Web.UI.Page
+    {
+        private string _currentUserName;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            _currentUserName = User.Identity.GetUserName();
+        }
+
+        public IQueryable<Activity> GetCurrentActivities()
+        {
+            var db = new UserOperationsContext();
+            var activitiesId = db.Reports.Where(r => r.WorkerUserName == _currentUserName).Select(r => r.ActivityId);
+            var activities = db.Activities.Where(a => activitiesId.Contains(a.ActivityId));
+            return activities;
+        }
+
+        public IQueryable<Activity> GetPendingActivities()
+        {
+            var db = new UserOperationsContext();
+            var activities = db.UsersData.First(u => u.UserName == _currentUserName).PendingActivity.AsQueryable();
+            return activities;
+        }
+
+        protected void btnCreateActivity_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("/MyVale/ActivityCreate");
+        }
+
+        protected void grdCurrentActivities_RowCommand(object sender, GridViewCommandEventArgs e)
+        {
+            if (e.CommandName == "ViewDetails")
+            {
+                int index = Convert.ToInt32(e.CommandArgument);
+                int activityId = Convert.ToInt32(grdCurrentActivities.Rows[index].Cells[0].Text);
+                Response.Redirect("/MyVale/ActivityDetails?activityId=" + activityId);
+            }
+
+        }
+
+        protected void grdPendingActivities_RowCommand(object sender, GridViewCommandEventArgs e)
+        {
+            int index = Convert.ToInt32(e.CommandArgument);
+            int activityId = Convert.ToInt32(grdPendingActivities.Rows[index].Cells[0].Text);
+            var db = new UserOperationsContext();
+            var activity = db.Activities.First(a => a.ActivityId == activityId);
+            var user = db.UsersData.First(u => u.UserName == _currentUserName);
+            if (e.CommandName == "AcceptActivity")
+            {
+                db.Reports.Add(new ActivityReport 
+                { 
+                    ActivityId = activityId,
+                    WorkerUserName = _currentUserName, 
+                    HoursWorked = 0,
+                    Date = DateTime.Today, 
+                    ActivityDescription = "Accepted activity from another user"
+                });
+            }
+            user.PendingActivity.Remove(activity);
+            db.SaveChanges();
+            grdCurrentActivities.DataBind();
+            grdPendingActivities.DataBind();
+        }
+
+
+        protected void btnExportCSV_Click(object sender, EventArgs e)
+        {
+            var db = new UserOperationsContext();
+            ExportToCSV(new List<string>() { _currentUserName });
+           
+        }
+
+        public void ExportToCSV(List<string> usersNames)
+        {
+            List<Activity> activities;
+            using (var activityActions = new ActivityActions())
+            {
+                string name = usersNames.Count == 1 ? usersNames[0] : "Gruppo Utenti";
+                Response.AddHeader("content-disposition", string.Format("attachment; filename=ListActivities({0}).csv", name));
+                Response.ContentType = "application/text";
+                StringBuilder strbldr = new StringBuilder();
+                //separting header columns text with comma operator
+                strbldr.Append("Utente;Id;Nome Attività;Data Inizio;Data Fine;Ore Di Lavoro");
+                //appending new line for gridview header row
+                strbldr.Append("\n");
+                foreach (var userName in usersNames)
+                {
+                    activities = activityActions.GetActivities(userName, ActivityStatus.Ongoing);
+                    foreach (var activity in activities)
+                    {
+                        strbldr.Append(userName + ';');
+                        strbldr.Append(activity.ActivityId.ToString() + ';');
+                        strbldr.Append(activity.ActivityName + ';');
+                        strbldr.Append(activity.StartDate.Value.ToShortDateString() + ';');
+                        if (activity.ExpireDate.HasValue)
+                            strbldr.Append(activity.ExpireDate.Value.ToShortDateString() + ';');
+                        else
+                            strbldr.Append("Non definito;");
+                        strbldr.Append(activityActions.GetHoursWorked(userName, activity.ActivityId).ToString() + ';');
+                        strbldr.Append("\n");
+                    }
+                }
+                Response.Write(strbldr.ToString());
+                Response.End();
+            }
+        }
+    }
+}
---
+++ VALE/VALE/MyVale/EventDetails.aspx
@@ -0,0 +1,39 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="EventDetails.aspx.cs" Inherits="VALE.MyVale.EventDetails" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+    <asp:FormView runat="server" ID="EventDetail" ItemType="VALE.Models.Event" SelectMethod="GetEvent">
+        <ItemTemplate>
+            <h3><%#: Item.Name %></h3>
+            <asp:Button ID="btnAttend" Text="I will attend" runat="server" OnClick="btnAttend_Click" /><br />
+            <h4>Event details</h4>
+            <asp:Label runat="server"><%#: String.Format("When: {0}", Item.EventDate.ToShortDateString()) %></asp:Label>
+            <br />
+            <asp:Label runat="server"><%#: String.Format("Created by: {0}", Item.Organizer.FullName) %></asp:Label>
+            <br />
+            <asp:Label runat="server"><%#: Item.Public ? "Public event" : "Private event" %></asp:Label>
+            <br />
+            <asp:Label runat="server"><%#: String.Format("Description: {0}", Item.Description) %></asp:Label><br />
+            <h4>Registered users</h4>
+            <asp:GridView ItemType="VALE.Models.UserData" AutoGenerateColumns="false" GridLines="Both" AllowSorting="true"
+                SelectMethod="GetRegisteredUsers" runat="server" ID="lstUsers" CssClass="table table-striped table-bordered">
+                <Columns>
+                    <asp:BoundField DataField="FullName" HeaderText="Name" SortExpression="FullName" />
+                    <asp:BoundField DataField="Email" HeaderText="Email" SortExpression="Email" />
+                </Columns>
+                <EmptyDataTemplate>
+                    <asp:Label runat="server">No registered users</asp:Label>
+                </EmptyDataTemplate>
+            </asp:GridView>
+            <br />
+            <h4>Related project</h4>
+            <asp:FormView runat="server" ID="ProjectDetail" ItemType="VALE.Models.Project" SelectMethod="GetRelatedProject">
+                <ItemTemplate>
+                    <a href="ProjectDetails.aspx?projectId=<%#:Item.ProjectId %>"><%#: Item.ProjectName %></a>
+                    <br />
+                </ItemTemplate>
+            </asp:FormView>
+            <h4>Documents</h4>
+            <asp:ListBox runat="server" CssClass="form-control" Width="400px" ID="lstDocuments" SelectMethod="GetRelatedDocuments"></asp:ListBox>
+            <asp:Button runat="server" Text="View document" CssClass="btn btn-info" ID="btnViewDocument" OnClick="btnViewDocument_Click" />
+        </ItemTemplate>
+    </asp:FormView>
+</asp:Content>
