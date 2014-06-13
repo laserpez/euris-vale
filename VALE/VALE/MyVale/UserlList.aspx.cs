f2fcd8215d124011dab982cd147459bb58a6cb13
Federico Liuzzi <liuzzi.federico@gmail.com>
2014-06-13 10:08:58 +0200

    Aggiunta pagina userlist, dimenticata prima.

---
+++ VALE/VALE/MyVale/UserlList.aspx.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using VALE.Models;
+
+namespace VALE.MyVale
+{
+    public partial class UserlList : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        public IQueryable<ApplicationUser> GetUsers()
+        {
+            var db = new ApplicationDbContext();
+            return db.Users;
+        }
+    }
+}
