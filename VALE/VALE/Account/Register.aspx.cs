377d28cc23bf27773018f135608fd8971b563395
Federico Liuzzi <liuzzi.federico@gmail.com>
2014-06-13 10:08:35 +0200

    Sistemata pagina USerList. Aggiunto numeri di telefono.

---
+++ VALE/VALE/Account/Register.aspx.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using Owin;
+using VALE.Models;
+using System.Collections.Generic;
+using VALE.StateInfo;
+using System.Text.RegularExpressions;
+
+namespace VALE.Account
+{
+    public partial class Register : Page
+    {
+
+        private void SetRegionDropDownList()
+        {
+            List<string> init = new List<string> { "Select" };
+            DropDownProvince.DataSource = init;
+            DropDownProvince.DataBind();
+            DropDownCity.DataSource = init;
+            DropDownCity.DataBind();
+            String path = Server.MapPath("~/StateInfo/regioni_province_comuni.xml");
+            StateInfoXML.GetInstance().FileName = path;
+            var list = StateInfoXML.GetInstance().LoadData();
+            var regions = (from r in list where r.depth == "0" orderby r.name select r.name).ToList();
+            regions.Insert(0, "Select");
+            DropDownRegion.DataSource = regions;
+            DropDownRegion.DataBind();
+        }
+
+        protected void Region_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (DropDownRegion.SelectedIndex == 0)
+            {
+                List<string> init = new List<string> { "Select" };
+                DropDownProvince.DataSource = init;
+                DropDownProvince.DataBind();
+                DropDownCity.DataSource = init;
+                DropDownCity.DataBind();
+            }
+            else
+            {
+                var list = StateInfoXML.GetInstance().LoadData();
+                var tid = (from r in list where r.depth == "0" && r.name == DropDownRegion.SelectedValue select r.tid).FirstOrDefault();
+                var provinces = (from r in list where r.depth == "1" && r.parent == tid orderby r.name select r.name).ToList();
+                provinces.Insert(0, "Select");
+                DropDownProvince.DataSource = provinces;
+                DropDownProvince.DataBind();
+            }
+        }
+
+        protected void State_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (DropDownProvince.SelectedIndex == 0)
+            {
+                List<string> init = new List<string> { "Select" };
+                DropDownCity.DataSource = init;
+                DropDownCity.DataBind();
+            }
+            else
+            {
+                var list = StateInfoXML.GetInstance().LoadData();
+                var tid = (from r in list where r.depth == "1" && r.name == DropDownProvince.SelectedValue select r.tid).FirstOrDefault();
+                var citys = (from r in list where r.depth == "2" && r.parent == tid orderby r.name select r.name).ToList();
+                citys.Insert(0, "Select");
+                DropDownCity.DataSource = citys;
+                DropDownCity.DataBind();
+            }
+        }
+
+        protected void CreateUser_Click(object sender, EventArgs e)
+        {
+
+
+            var db = new UserOperationsContext();
+            var manager = Context.GetOwinContext().GetUserManager<ApplicationUserManager>();
+            var user = new ApplicationUser()
+            {
+                UserName = TextUserName.Text,
+                FirstName = TextFirstName.Text,
+                LastName = TextLastName.Text,
+                Address = TextAddress.Text,
+                Telephone = TextTelephone.Text,
+                CellPhone = TextCellPhone.Text,
+                Region = DropDownRegion.SelectedValue,
+                Province = DropDownProvince.SelectedValue,
+                City = DropDownCity.SelectedValue,
+                CF = TextCF.Text,
+                NeedsApproval = checkAssociated.Checked,
+                Email = Email.Text
+            };
+            var passwordValidator = new PasswordValidator();
+            //per la password sono richiesti solo sei caratteri
+            passwordValidator.RequiredLength = 6;
+            manager.PasswordValidator = passwordValidator;
+
+            IdentityResult result = manager.Create(user, Password.Text);
+            if (result.Succeeded)
+            {
+                db.UsersData.Add(new UserData
+                {
+                    UserName = user.UserName,
+                    Email = user.Email,
+                    FullName = user.FirstName + " " + user.LastName
+                });
+                db.SaveChanges();
+                IdentityHelper.SignIn(manager, user, isPersistent: false);
+
+                // For more information on how to enable account confirmation and password reset please visit http://go.microsoft.com/fwlink/?LinkID=320771
+                //string code = manager.GenerateEmailConfirmationToken(user.Id);
+                //string callbackUrl = IdentityHelper.GetUserConfirmationRedirectUrl(code, user.Id);
+                //manager.SendEmail(user.Id, "Confirm your account", "Please confirm your account by clicking <a href=\"" + callbackUrl + "\">here</a>.");
+
+                IdentityHelper.RedirectToReturnUrl(Request.QueryString["ReturnUrl"], Response);
+            }
+            else
+            {
+                ErrorMessage.Text = result.Errors.FirstOrDefault();
+            }
+        }
+    }
+}
---
+++ VALE/VALE/Models/UserOperationsContext.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Web;
+
+namespace VALE.Models
+{
+    public class UserOperationsContext :DbContext
+    {
+        public UserOperationsContext() : base("VALEData") { }
+
+        public DbSet<Event> Events { get; set; }
+        public DbSet<Activity> Activities { get; set; }
+        public DbSet<Project> Projects { get; set; }
+        public DbSet<UserData> UsersData { get; set; }
+        public DbSet<ActivityReport> Reports { get; set; }
+        public DbSet<Intervention> Interventions { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+        public DbSet<BlogArticle> BlogArticles { get; set; }
+        public DbSet<BlogComment> BlogComments { get; set; }
+        public DbSet<BODReport> BODReports { get; set; }
+
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Event>().HasOptional(e => e.RelatedProject).WithMany(p => p.Events).WillCascadeOnDelete();
+            
+            modelBuilder.Entity<Event>()
+                .HasMany(u => u.RegisteredUsers)
+                .WithMany(e => e.AttendingEvents)
+                .Map(m =>
+                    {
+                        m.MapLeftKey("EventId");
+                        m.MapRightKey("UserDataId");
+                        m.ToTable("EventsUsers");
+                    });
+
+            modelBuilder.Entity<Project>()
+                .HasMany(u => u.InvolvedUsers)
+                .WithMany(e => e.AttendingProjects)
+                .Map(m =>
+                {
+                    m.MapLeftKey("ProjectId");
+                    m.MapRightKey("UserDataId");
+                    m.ToTable("ProjectsUsers");
+                });
+
+            modelBuilder.Entity<Activity>().HasOptional(a => a.RelatedProject).WithMany(p => p.Activities).WillCascadeOnDelete();
+            modelBuilder.Entity<Activity>()
+                .HasMany(a => a.PendingUsers)
+                .WithMany(u => u.PendingActivity)
+                .Map(m =>
+                {
+                    m.MapLeftKey("ActivityId");
+                    m.MapRightKey("UserDataId");
+                    m.ToTable("PendingActivitiesUsers");
+                });
+
+        }
+    }
+}
