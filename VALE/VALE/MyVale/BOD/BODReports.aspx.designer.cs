80d10b8252113fb801f16ff6bae6b827eedccaae
Elena Bracchi <erika.dreven@gmail.com>
2014-06-24 16:54:18 +0200

    Manca da modificare il GetSortedData in PersonalActivities e BORDReport mancano iconcine.

---
+++ VALE/VALE/MyVale/BOD/BODReports.aspx.designer.cs
@@ -0,0 +1,42 @@
+﻿//------------------------------------------------------------------------------
+// <generato automaticamente>
+//     Codice generato da uno strumento.
+//
+//     Le modifiche a questo file possono causare un comportamento non corretto e verranno perse se
+//     il codice viene rigenerato. 
+// </generato automaticamente>
+//------------------------------------------------------------------------------
+
+namespace VALE.MyVale.BOD {
+    
+    
+    public partial class BODReports {
+        
+        /// <summary>
+        /// Controllo UpdatePanel1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generato automaticamente.
+        /// Per la modifica, spostare la dichiarazione di campo dal file di progettazione al file code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.UpdatePanel UpdatePanel1;
+        
+        /// <summary>
+        /// Controllo HeaderName.
+        /// </summary>
+        /// <remarks>
+        /// Campo generato automaticamente.
+        /// Per la modifica, spostare la dichiarazione di campo dal file di progettazione al file code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label HeaderName;
+        
+        /// <summary>
+        /// Controllo grdBODReport.
+        /// </summary>
+        /// <remarks>
+        /// Campo generato automaticamente.
+        /// Per la modifica, spostare la dichiarazione di campo dal file di progettazione al file code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.GridView grdBODReport;
+    }
+}
