cb6eb52f699c002925ca2c4b1d38ce65d983236c
Elena Bracchi <erika.dreven@gmail.com>
2014-06-24 13:11:31 +0200

    Modifiche varie, aggiunti frame a varie pagine.

---
+++ VALE/VALE/Admin/EventReport.aspx.designer.cs
@@ -0,0 +1,24 @@
+﻿//------------------------------------------------------------------------------
+// <generato automaticamente>
+//     Codice generato da uno strumento.
+//
+//     Le modifiche a questo file possono causare un comportamento non corretto e verranno perse se
+//     il codice viene rigenerato. 
+// </generato automaticamente>
+//------------------------------------------------------------------------------
+
+namespace VALE.Admin {
+    
+    
+    public partial class EventReport {
+        
+        /// <summary>
+        /// Controllo EventDetail.
+        /// </summary>
+        /// <remarks>
+        /// Campo generato automaticamente.
+        /// Per la modifica, spostare la dichiarazione di campo dal file di progettazione al file code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.FormView EventDetail;
+    }
+}
