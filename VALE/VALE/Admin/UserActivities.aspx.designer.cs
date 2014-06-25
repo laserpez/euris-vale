03ff8ad770e2ac0927b443c1b610bb45837ed015
roberto montinaro <roberto.montinaro@gmail.com>
2014-06-25 13:18:34 +0200

    Sistemati USerActvities e UserInterventions. sistemato BODReports (GridView)

--- VALE/VALE/Admin/UserActivities.aspx.designer.cs
+++ VALE/VALE/Admin/UserActivities.aspx.designer.cs
@@ -31,15 +31,6 @@ namespace VALE.Admin {
         protected global::System.Web.UI.WebControls.Label HeaderName;
         
         /// <summary>
-        /// lblSummary control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Label lblSummary;
-        
-        /// <summary>
         /// grdReports control.
         /// </summary>
         /// <remarks>
