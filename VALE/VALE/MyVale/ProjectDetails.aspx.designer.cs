27932e416b5542f8d776c863fe07b4112c8aab4e
emmacoradduzza@gmail.com <emmacoradduzza@gmail.com>
2014-06-26 11:58:34 +0200

    commit emma: controllo e rifinitura della parte grafica

---
+++ VALE/VALE/MyVale/ProjectDetails.aspx.designer.cs
@@ -0,0 +1,33 @@
+﻿//------------------------------------------------------------------------------
+// <generato automaticamente>
+//     Codice generato da uno strumento.
+//
+//     Le modifiche a questo file possono causare un comportamento non corretto e verranno perse se
+//     il codice viene rigenerato. 
+// </generato automaticamente>
+//------------------------------------------------------------------------------
+
+namespace VALE.MyVale {
+    
+    
+    public partial class ProjectDetails {
+        
+        /// <summary>
+        /// Controllo HeaderName.
+        /// </summary>
+        /// <remarks>
+        /// Campo generato automaticamente.
+        /// Per la modifica, spostare la dichiarazione di campo dal file di progettazione al file code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label HeaderName;
+        
+        /// <summary>
+        /// Controllo ProjectDetail.
+        /// </summary>
+        /// <remarks>
+        /// Campo generato automaticamente.
+        /// Per la modifica, spostare la dichiarazione di campo dal file di progettazione al file code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.FormView ProjectDetail;
+    }
+}
