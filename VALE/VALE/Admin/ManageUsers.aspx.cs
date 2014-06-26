f0115a8c4778844a5580263c3b4c41fa3a99062a
Federico Liuzzi <liuzzi.federico@gmail.com>
2014-06-26 15:02:10 +0200

    activityActions. ExportToCSV

---
+++ VALE/VALE/Admin/ManageUsers.aspx.cs
@@ -0,0 +1,246 @@
+﻿//using Microsoft.Ajax.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using VALE.Logic;
+using VALE.Models;
+
+namespace VALE.Admin
+{
+    public partial class ManageUsers : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                grdUsers.Columns[7].Visible = false;
+                
+                var lstUsers = GetUsers();
+                grdUsers.DataSource = lstUsers;
+                grdUsers.DataBind();
+            }
+
+            if (GetWaitingUsers().Count() == 0)
+                btnConfirmUser.Enabled = false;
+
+        }
+
+        public IQueryable<ApplicationUser> GetWaitingUsers()
+        {
+            var db = new ApplicationDbContext();
+            var users = db.Users.Where(u => u.NeedsApproval == true);
+            return users;
+        }
+
+        public List<ApplicationUser> GetUsers()
+        {
+            var db = new ApplicationDbContext();
+            var users = db.Users.ToList();
+            return users;
+        }
+
+        public string GetRoleName(string userId)
+        {
+            var db = new ApplicationDbContext();
+            var user = db.Users.First(u => u.Id == userId);
+            if (user.Roles.Count != 0)
+            {
+                var roleId = user.Roles.First().RoleId;
+                var roleName = db.Roles.FirstOrDefault(o => o.Id == roleId).Name;
+                return roleName;
+            }
+            else
+                return "Utente";
+        }
+
+        protected void btnConfimUser_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < grdUsers.Rows.Count; i++)
+            {
+                CheckBox chkBox = (CheckBox)grdUsers.Rows[i].FindControl("chkSelectUser");
+                if (chkBox.Checked)
+                {
+                    string userName = ((Label)grdUsers.Rows[i].Cells[0].FindControl("labelUserName")).Text;
+                    AdminActions.ConfirmUser(userName);
+
+                    //MailHelper.SendMail(WaitingUsers.Rows[i].Cells[1].Text, "Your associated account has been confirmed", "Account confirmed");
+                }
+            }
+            grdUsers.DataBind();
+        }
+
+        protected void btnChangeUser_Click(object sender, EventArgs e)
+        {
+            LinkButton button = (LinkButton)sender;
+            if (button.Text == "Amministratore")
+            {
+                lblChangeRole.Text = UserActions.ChangeUserRole(button.CommandArgument, "Amministratore");
+                if (lblChangeRole.Text != "")
+                    lblChangeRole.Visible = true;
+            }
+            else if (button.Text == "Membro del consiglio")
+            {
+                lblChangeRole.Text = UserActions.ChangeUserRole(button.CommandArgument, "Membro del consiglio");
+                if (lblChangeRole.Text != "")
+                    lblChangeRole.Visible = true;
+            }
+            else if (button.Text == "Socio")
+            {
+                lblChangeRole.Text = UserActions.ChangeUserRole(button.CommandArgument, "Socio");
+                if (lblChangeRole.Text != "")
+                    lblChangeRole.Visible = true;
+            }
+            Response.Redirect("~/Admin/ManageUsers");
+        }
+
+        private void PreparePanelForRegistrationRequest()
+        {
+            grdUsers.Columns[8].Visible = false;
+            grdUsers.Columns[7].Visible = true;
+            NotificationNumber.Visible = true;
+            btnConfirmUser.Visible = true;
+            HeaderName.Text = " Richieste Di Registrazione";
+        }
+
+        private void PreparePanelForManage()
+        {
+            grdUsers.Columns[8].Visible = true;
+            grdUsers.Columns[7].Visible = false;
+            NotificationNumber.Visible = false;
+            btnConfirmUser.Visible = false;
+            HeaderName.Text = " Gestione Utenti";
+
+            GetAllUsersButton.Visible = false;
+            GetAdminButton.Visible = false;
+            GetPartnersButton.Visible = false;
+            GetDirectivPartnersButton.Visible = false;
+            GetRequestsdButton.Visible = false;
+        }
+
+        protected void GetAllUsers_Click(object sender, EventArgs e)
+        {
+            PreparePanelForManage();
+            ListUsersType.Text = "Tutti";
+            GetAllUsersButton.Visible = true;
+            LoadData();
+        }
+
+        protected void GetPartners_Click(object sender, EventArgs e)
+        {
+            PreparePanelForManage();
+            ListUsersType.Text = "Soci";
+            GetPartnersButton.Visible = true;
+            LoadData();
+        }
+
+        protected void GetAdmin_Click(object sender, EventArgs e)
+        {
+            PreparePanelForManage();
+            ListUsersType.Text = "Amministratori";
+            GetAdminButton.Visible = true;
+            LoadData();
+        }
+
+        protected void GetDirectivPartners_Click(object sender, EventArgs e)
+        {
+            PreparePanelForManage();
+            ListUsersType.Text = "Membri";
+            GetDirectivPartnersButton.Visible = true;
+            LoadData();
+        }
+
+        protected void GetRequests_Click(object sender, EventArgs e)
+        {
+            PreparePanelForRegistrationRequest();
+            GetAllUsersButton.Visible = false;
+            GetAdminButton.Visible = false;
+            GetPartnersButton.Visible = false;
+            GetDirectivPartnersButton.Visible = false;
+            ListUsersType.Text = "Richieste";
+            GetRequestsdButton.Visible = true;
+            LoadData();
+        }
+        private void LoadData()
+        {
+            var db = new ApplicationDbContext();
+            List<ApplicationUser> list = new List<ApplicationUser>();
+
+            switch (ListUsersType.Text)
+            {
+                case "Tutti":
+                    list = db.Users.ToList();
+                    break;
+                case "Amministratori":
+                    var rolesA = db.Roles.Where(p => p.Name == "Amministratore").Select(k => k.Id).FirstOrDefault();
+                    list = db.Users.Where(o => o.Roles.Select(k => k.RoleId).FirstOrDefault() == rolesA).ToList();
+                    break;
+                case "Soci":
+                    var rolesS = db.Roles.Where(p => p.Name == "Socio").Select(k => k.Id).FirstOrDefault();
+                    list = db.Users.Where(o => o.Roles.Select(k => k.RoleId).FirstOrDefault() == rolesS ).ToList();
+                    break;
+                case "Membri":
+                    var rolesM = db.Roles.Where(p => p.Name == "Membro del consiglio").Select(k => k.Id).FirstOrDefault();
+                    list = db.Users.Where(o => o.Roles.Select(k => k.RoleId).FirstOrDefault() == rolesM).ToList();
+                    break;
+                case "Richieste":
+                    list = db.Users.Where(u => u.NeedsApproval == true).ToList();
+                    break;
+                default:
+                    break;
+            }
+                    grdUsers.DataSource = list;
+                    grdUsers.DataBind();
+        }
+
+        protected void grdUsers_Sorting(object sender, GridViewSortEventArgs e)
+        {
+            if (GridViewSortDirection == SortDirection.Ascending)
+                GridViewSortDirection = SortDirection.Descending;
+            else
+                GridViewSortDirection = SortDirection.Ascending;
+
+            grdUsers.DataSource = GetSortedData(e.SortExpression);
+            grdUsers.DataBind();
+        }
+
+        private List<ApplicationUser> GetSortedData(string sortExpression)
+        {
+            var result = GetUsers();
+            if (sortExpression != "Ruolo")
+            {
+                var param = Expression.Parameter(typeof(ApplicationUser), sortExpression);
+                var sortBy = Expression.Lambda<Func<ApplicationUser, object>>(Expression.Convert(Expression.Property(param, sortExpression), typeof(object)), param);
+
+                if (GridViewSortDirection == SortDirection.Descending)
+                    result = result.AsQueryable<ApplicationUser>().OrderByDescending(sortBy).ToList();
+                else
+                    result = result.AsQueryable<ApplicationUser>().OrderBy(sortBy).ToList();
+            }
+            else
+            {
+                if (GridViewSortDirection == SortDirection.Descending)
+                    result = result.AsQueryable<ApplicationUser>().OrderByDescending(u => GetRoleName(u.Id)).ToList();
+                else
+                    result = result.AsQueryable<ApplicationUser>().OrderBy(u => GetRoleName(u.Id)).ToList();
+            }
+
+            return result;
+        }
+
+        public SortDirection GridViewSortDirection
+        {
+            get
+            {
+                if (ViewState["sortDirection"] == null)
+                    ViewState["sortDirection"] = SortDirection.Ascending;
+
+                return (SortDirection)ViewState["sortDirection"];
+            }
+            set { ViewState["sortDirection"] = value; }
+        }
+    }
+}
---
+++ VALE/VALE/MyVale/Activities.aspx.cs
@@ -0,0 +1,225 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Microsoft.AspNet.Identity;
+using VALE.Models;
+using CsvHelper;
+using System.IO;
+using System.Text;
+using VALE.Logic;
+using System.Web.ModelBinding ;
+
+namespace VALE.MyVale
+{
+    public partial class Activities : System.Web.UI.Page
+    {
+        private string _currentUserName;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            _currentUserName = User.Identity.GetUserName();
+            if (!IsPostBack)
+            {
+                filterPanel.Visible = false;
+            }
+        }
+
+        public string GetStatus(Activity anActivity)
+        {
+            string status;
+            using (var activityActions = new ActivityActions())
+            {
+                status = activityActions.GetStatus(anActivity);
+            }
+            return status;
+            //if (anActivity.Status == ActivityStatus.ToBePlanned)
+            //    return "Da pianificare";
+            //if (anActivity.Status == ActivityStatus.Suspended)
+            //    return "Sospesa";
+            //if (anActivity.Status == ActivityStatus.Ongoing)
+            //    return "In corso";
+            //if (anActivity.Status == ActivityStatus.Done)
+            //    return "Terminata";
+            //if (anActivity.Status == ActivityStatus.Deleted)
+            //    return "Cancellata";
+
+            //return null;
+        }
+
+        protected void Page_PreRender(object sender, EventArgs e)
+        {
+            ShowHideControls();
+        }
+
+        private void ShowHideControls()
+        {
+            if (grdCurrentActivities.Rows.Count == 0 && grdPendingActivities.Rows.Count == 0)
+            {
+                filterPanel.Visible = false;
+                btnShowFilters.Visible = false;
+                ExternalPanelDefault.Visible = false;
+                InternalPanelHeading.Visible = false;
+                btnExportCSV.Visible = false;
+            }
+        }
+
+        public IQueryable<Activity> GetCurrentActivities([Control]string txtName, [Control]string txtDescription, [Control]string ddlStatus)
+        {
+            IQueryable<Activity> activities;
+            using (var activityActions = new ActivityActions())
+            {
+                activities = activityActions.GetCurrentActivities(txtName,txtDescription,ddlStatus,_currentUserName);
+            }
+            return activities;
+            //var db = new UserOperationsContext();
+            //var activitiesId = db.Reports.Where(r => r.WorkerUserName == _currentUserName).Select(r => r.ActivityId);
+            //var activities = db.Activities.Where(a => activitiesId.Contains(a.ActivityId));
+            //if (txtName != null)
+            //    activities = activities.Where(a => a.ActivityName.ToUpper().Contains(txtName.ToUpper()));
+            //if(txtDescription != null)
+            //    activities = activities.Where(a => a.Description.ToUpper().Contains(txtDescription.ToUpper()));
+            //if(ddlStatus != null && ddlStatus != "Tutte")
+            //{
+            //    ActivityStatus statusFilter = ActivityStatus.Deleted;
+            //    switch(ddlStatus)
+            //    {
+            //        case "Da pianificare":
+            //            statusFilter = ActivityStatus.ToBePlanned;
+            //            break;
+            //        case "In corso":
+            //            statusFilter = ActivityStatus.Ongoing;
+            //            break;
+            //        case "Sospese":
+            //            statusFilter = ActivityStatus.Suspended;
+            //            break;
+            //        case "Terminata":
+            //            statusFilter = ActivityStatus.Done;
+            //            break;
+            //    }
+            //    activities = activities.Where(a => a.Status == statusFilter);
+            //}
+            //return activities;
+        }
+
+        public IQueryable<Activity> GetPendingActivities()
+        {
+            IQueryable<Activity> activities;
+            using(var activityActions = new ActivityActions())
+            {
+                activities = activityActions.GetPendingActivities(_currentUserName);
+            }
+            return activities;
+            //var db = new UserOperationsContext();
+            //var activities = db.UsersData.First(u => u.UserName == _currentUserName).PendingActivity.AsQueryable();
+            //return activities;
+        }
+
+        protected void btnCreateActivity_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("/MyVale/ActivityCreate");
+        }
+
+        protected void grdCurrentActivities_RowCommand(object sender, GridViewCommandEventArgs e)
+        {
+            if (e.CommandName == "ViewDetails")
+            {
+                int index = Convert.ToInt32(e.CommandArgument);
+                int activityId = (int)grdCurrentActivities.DataKeys[index].Value;
+                Response.Redirect("/MyVale/ActivityDetails?activityId=" + activityId);
+            }
+        }
+
+        protected void grdPendingActivities_RowCommand(object sender, GridViewCommandEventArgs e)
+        {
+            int index = Convert.ToInt32(e.CommandArgument);
+            int activityId = Convert.ToInt32(grdPendingActivities.DataKeys[index].Value);
+            var db = new UserOperationsContext();
+            var activity = db.Activities.First(a => a.ActivityId == activityId);
+            var user = db.UsersData.First(u => u.UserName == _currentUserName);
+            if (e.CommandName == "AcceptActivity")
+            {
+                db.Reports.Add(new ActivityReport 
+                { 
+                    ActivityId = activityId,
+                    WorkerUserName = _currentUserName, 
+                    HoursWorked = 0,
+                    Date = DateTime.Today, 
+                    ActivityDescription = "Attività accettata da un altro utente."
+                });
+            }
+            user.PendingActivity.Remove(activity);
+            db.SaveChanges();
+            grdCurrentActivities.DataBind();
+            grdPendingActivities.DataBind();
+        }
+
+        protected void btnExportCSV_Click(object sender, EventArgs e)
+        {
+            var db = new UserOperationsContext();
+            ExportToCSV(new List<string>() { _currentUserName });
+        }
+
+        public void ExportToCSV(List<string> usersNames)
+        {
+            using (var exportToCSV = new ExportToCSV())
+            {
+                string name = usersNames.Count == 1 ? usersNames[0] : "Gruppo Utenti";
+                Response.AddHeader("content-disposition", string.Format("attachment; filename=ListActivities({0}).csv", name));
+                Response.ContentType = "application/text";
+                StringBuilder strbldr = exportToCSV.ExportActivities(usersNames);
+                //List<Activity> activities;
+                //StringBuilder strbldr = new StringBuilder();
+                ////separting header columns text with comma operator
+                //strbldr.Append("Utente;Id;Nome Attività;Data Inizio;Data Fine;Ore Di Lavoro;Stato");
+                ////appending new line for gridview header row
+                //strbldr.Append("\n");
+                //foreach (var userName in usersNames)
+                //{
+                //    activities = activityActions.GetActivities(userName);
+                //    foreach (var activity in activities)
+                //    {
+                //        strbldr.Append(userName + ';');
+                //        strbldr.Append(activity.ActivityId.ToString() + ';');
+                //        strbldr.Append(activity.ActivityName + ';');
+                //        strbldr.Append(activity.StartDate.Value.ToShortDateString() + ';');
+                //        if (activity.ExpireDate.HasValue)
+                //            strbldr.Append(activity.ExpireDate.Value.ToShortDateString() + ';');
+                //        else
+                //            strbldr.Append("Non definito;");
+                //        strbldr.Append(activityActions.GetHoursWorked(userName, activity.ActivityId).ToString() + ';');
+                //        strbldr.Append(activity.Status + ';');
+                //        strbldr.Append("\n");
+                //    }
+                //}
+                Response.Write(strbldr.ToString());
+                Response.End();
+            }
+        }
+
+        protected void btnShowFilters_Click(object sender, EventArgs e)
+        {
+            filterPanel.Visible = !filterPanel.Visible;
+        }
+
+        protected void btnFilterProjects_Click(object sender, EventArgs e)
+        {
+            grdCurrentActivities.DataBind();
+        }
+
+        protected void btnClearFilters_Click(object sender, EventArgs e)
+        {
+            txtDescription.Text = "";
+            txtName.Text = "";
+            ddlStatus.SelectedValue = "Tutte";
+            grdCurrentActivities.DataBind();
+        }
+
+        public List<string> PopulateDropDown()
+        {
+            return new List<string>() { "Tutte", "Da pianificare", "In corso", "Sospese", "Terminate" };
+        }
+    }
+}
