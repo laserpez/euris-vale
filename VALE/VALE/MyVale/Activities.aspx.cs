04763f67645f020ff4398fc0952446f5f94da6f4
Federico Liuzzi <liuzzi.federico@gmail.com>
2014-06-26 15:04:37 +0200

    pulizia codice

--- VALE/VALE/MyVale/Activities.aspx.cs
+++ VALE/VALE/MyVale/Activities.aspx.cs
@@ -170,30 +170,6 @@ namespace VALE.MyVale
                 Response.AddHeader("content-disposition", string.Format("attachment; filename=ListActivities({0}).csv", name));
                 Response.ContentType = "application/text";
                 StringBuilder strbldr = exportToCSV.ExportActivities(usersNames);
-                //List<Activity> activities;
-                //StringBuilder strbldr = new StringBuilder();
-                ////separting header columns text with comma operator
-                //strbldr.Append("Utente;Id;Nome Attività;Data Inizio;Data Fine;Ore Di Lavoro;Stato");
-                ////appending new line for gridview header row
-                //strbldr.Append("\n");
-                //foreach (var userName in usersNames)
-                //{
-                //    activities = activityActions.GetActivities(userName);
-                //    foreach (var activity in activities)
-                //    {
-                //        strbldr.Append(userName + ';');
-                //        strbldr.Append(activity.ActivityId.ToString() + ';');
-                //        strbldr.Append(activity.ActivityName + ';');
-                //        strbldr.Append(activity.StartDate.Value.ToShortDateString() + ';');
-                //        if (activity.ExpireDate.HasValue)
-                //            strbldr.Append(activity.ExpireDate.Value.ToShortDateString() + ';');
-                //        else
-                //            strbldr.Append("Non definito;");
-                //        strbldr.Append(activityActions.GetHoursWorked(userName, activity.ActivityId).ToString() + ';');
-                //        strbldr.Append(activity.Status + ';');
-                //        strbldr.Append("\n");
-                //    }
-                //}
                 Response.Write(strbldr.ToString());
                 Response.End();
             }
