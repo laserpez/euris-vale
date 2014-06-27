c72346113328b1df3c749ab91ae55f2a8cc31795
Domenico Moreschini <domenico.moreschini@gmail.com>
2014-06-27 17:58:26 +0200

    Fix creazione BODReportCreate

--- VALE/VALE/Logic/BODReportActions.cs
+++ VALE/VALE/Logic/BODReportActions.cs
@@ -6,26 +6,26 @@ using VALE.Models;
 
 namespace VALE.Logic
 {
-    public class ReportActions : IActions
+    public class BODReportActions : IActions
     {
         public List<T> GetSortedData<T>(string sortExpression, System.Web.UI.WebControls.SortDirection direction, List<T> data)
         {
-            throw new NotImplementedException();
+            return null;
         }
 
         public List<T> GetFilteredData<T>(Dictionary<string, string> filters, List<T> data)
         {
-            throw new NotImplementedException();
+            return null;
         }
 
         public bool AddOrRemoveUserData<T>(T data, Models.UserData user)
         {
-            throw new NotImplementedException();
+            return false;
         }
 
         public bool IsUserRelated(int dataId, string username)
         {
-            throw new NotImplementedException();
+            return false;
         }
 
         public bool AddAttachment(int dataId, Models.AttachedFile file)
@@ -33,8 +33,8 @@ namespace VALE.Logic
             try
             {
                 var db = new UserOperationsContext();
-                var project = db.Projects.First(p => p.ProjectId == dataId);
-                project.AttachedFiles.Add(file);
+                var report = db.BODReports.First(r => r.BODReportId == dataId);
+                report.AttachedFiles.Add(file);
                 db.SaveChanges();
                 return true;
             }
@@ -66,6 +66,12 @@ namespace VALE.Logic
             var db = new UserOperationsContext();
             var report = db.BODReports.First(r => r.BODReportId == dataId);
             return report.AttachedFiles;
+        }
+
+
+        public bool RemoveAllAttachments(int dataId)
+        {
+            return false;
         }
     }
 }
