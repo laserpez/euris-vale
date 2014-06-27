9c3de5598e52a539b547d18171fe08b385806249
Domenico Moreschini <domenico.moreschini@gmail.com>
2014-06-27 15:02:17 +0200

    Aggiunta interfaccia a ProjectActions

---
+++ VALE/VALE/MyVale/Create/EventCreate.aspx.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Microsoft.AspNet.Identity;
+using VALE.Models;
+using System.IO;
+
+namespace VALE.MyVale
+{
+    public partial class EventCreate : System.Web.UI.Page
+    {
+        private string _currentUser;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            _currentUser = User.Identity.GetUserName();
+            if (!IsPostBack)
+            {
+                if (Request.QueryString["ProjectId"] != null)
+                    Session["callingProjectId"] = Request.QueryString["ProjectId"];
+                else
+                    Session["callingProjectId"] = null;
+                calendarFrom.StartDate = DateTime.Now;
+            }
+
+            if (Session["callingProjectId"] != null)
+            {
+                var db = new UserOperationsContext();
+                var projectId = Convert.ToInt32(Session["callingProjectId"].ToString());
+                var projectName = db.Projects.First(p => p.ProjectId == projectId).ProjectName;
+                SelectProject.DisableControl(projectName);
+            }
+        }
+
+        protected void btnSaveEvent_Click(object sender, EventArgs e)
+        {
+            var db = new UserOperationsContext();
+            var user = db.UsersData.FirstOrDefault(u => u.UserName == _currentUser);
+            var registeredUsers = new List<UserData>() { user };
+            var newEvent = new Event
+            {
+                Name = txtName.Text,
+                Description = txtDescription.Text,
+                Public = chkPublic.Checked,
+                EventDate = Convert.ToDateTime(txtStartDate.Text),
+                OrganizerUserName = _currentUser,
+                RegisteredUsers = registeredUsers,
+                RelatedProject = db.Projects.FirstOrDefault(p => p.ProjectName == SelectProject.ProjectNameTextBox.Text)
+            };
+            db.Events.Add(newEvent);
+            db.SaveChanges();
+
+            if (Session["callingProjectId"] != null)
+                Response.Redirect("/MyVale/ProjectDetails?projectId=" + Session["callingProjectId"].ToString());
+            else if (Session["requestFrom"] != null)
+                Response.Redirect(Session["requestFrom"].ToString());
+            else
+                Response.Redirect("/MyVale/Events");
+        }
+    }
+}
