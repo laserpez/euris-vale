1c58abdfb701fc431c42713fdc1faf4c4b720ce0
roberto montinaro <roberto.montinaro@gmail.com>
2014-07-01 17:31:08 +0200

    Inserito GroupActions.

---
+++ VALE/VALE/Account/RegisterExternalLogin.aspx.cs
@@ -0,0 +1,223 @@
+﻿using System;
+using System.Web;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin.Security;
+using Owin;
+using System.Linq;
+using VALE.Models;
+using System.Collections.Generic;
+using VALE.StateInfo;
+
+namespace VALE.Account
+{
+    public partial class RegisterExternalLogin : System.Web.UI.Page
+    {
+        protected string ProviderName
+        {
+            get { return (string)ViewState["ProviderName"] ?? String.Empty; }
+            private set { ViewState["ProviderName"] = value; }
+        }
+
+        protected string ProviderAccountKey
+        {
+            get { return (string)ViewState["ProviderAccountKey"] ?? String.Empty; }
+            private set { ViewState["ProviderAccountKey"] = value; }
+        }
+
+        private void RedirectOnFail()
+        {
+            Response.Redirect((User.Identity.IsAuthenticated) ? "~/Account/Manage" : "~/Account/Login");
+        }
+
+        protected void Page_Load()
+        {
+            // Process the result from an auth provider in the request
+            ProviderName = IdentityHelper.GetProviderNameFromRequest(Request);
+            if (String.IsNullOrEmpty(ProviderName))
+            {
+                RedirectOnFail();
+                return;
+            }
+            if (!IsPostBack)
+            {
+                SetRegionDropDownList();
+                var manager = Context.GetOwinContext().GetUserManager<ApplicationUserManager>();
+                var loginInfo = Context.GetOwinContext().Authentication.GetExternalLoginInfo();
+                if (loginInfo == null)
+                {
+                    RedirectOnFail();
+                    return;
+                }
+                var user = manager.Find(loginInfo.Login);
+                if (user != null)
+                {
+                    IdentityHelper.SignIn(manager, user, isPersistent: false);
+                    IdentityHelper.RedirectToReturnUrl(Request.QueryString["ReturnUrl"], Response);
+                }
+                else if (User.Identity.IsAuthenticated)
+                {
+                    // Apply Xsrf check when linking
+                    var verifiedloginInfo = Context.GetOwinContext().Authentication.GetExternalLoginInfo(IdentityHelper.XsrfKey, User.Identity.GetUserId());
+                    if (verifiedloginInfo == null)
+                    {
+                        RedirectOnFail();
+                        return;
+                    }
+
+                    var result = manager.AddLogin(User.Identity.GetUserId(), verifiedloginInfo.Login);
+                    // todo in casa devi inserire email e password da sito esterno
+                    if (result.Succeeded)
+                    {
+                        IdentityHelper.RedirectToReturnUrl(Request.QueryString["ReturnUrl"], Response);
+                    }
+                    else
+                    {
+                        AddErrors(result);
+                        return;
+                    }
+                }
+                else
+                {
+                    // todo in caso devi solo inserire l'email di fb
+                    // trovare\ aggiungere username
+                    TextUserName.Text = loginInfo.DefaultUserName;
+                    
+                }
+            }
+        }
+
+        private void ClearDropDownList()
+        {
+            List<string> init = new List<string> { "Seleziona" };
+            DropDownProvince.DataSource = init;
+            DropDownProvince.DataBind();
+            DropDownCity.DataSource = init;
+            DropDownCity.DataBind();
+        }
+
+        private void SetRegionDropDownList()
+        {
+            ClearDropDownList();
+            String path = Server.MapPath("~/StateInfo/regioni_province_comuni.xml");
+            StateInfoXML.GetInstance().FileName = path;
+            var list = StateInfoXML.GetInstance().LoadData();
+            var regions = (from r in list where r.depth == "0" orderby r.name select r.name).ToList();
+            regions.Insert(0, "Seleziona");
+            DropDownRegion.DataSource = regions;
+            DropDownRegion.DataBind();
+        }
+
+        protected void Region_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (DropDownRegion.SelectedIndex == 0)
+            {
+                List<string> init = new List<string> { "Seleziona" };
+                DropDownProvince.DataSource = init;
+                DropDownProvince.DataBind();
+                DropDownCity.DataSource = init;
+                DropDownCity.DataBind();
+            }
+            else
+            {
+                ClearDropDownList();
+
+                var list = StateInfoXML.GetInstance().LoadData();
+                var tid = (from r in list where r.depth == "0" && r.name == DropDownRegion.SelectedValue select r.tid).FirstOrDefault();
+                var provinces = (from r in list where r.depth == "1" && r.parent == tid orderby r.name select r.name).ToList();
+                provinces.Insert(0, "Seleziona");
+                DropDownProvince.DataSource = provinces;
+                DropDownProvince.DataBind();
+            }
+        }
+
+        protected void State_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (DropDownProvince.SelectedIndex == 0)
+            {
+                List<string> init = new List<string> { "Seleziona" };
+                DropDownCity.DataSource = init;
+                DropDownCity.DataBind();
+            }
+            else
+            {
+                var list = StateInfoXML.GetInstance().LoadData();
+                var tid = (from r in list where r.depth == "1" && r.name == DropDownProvince.SelectedValue select r.tid).FirstOrDefault();
+                var citys = (from r in list where r.depth == "2" && r.parent == tid orderby r.name select r.name).ToList();
+                citys.Insert(0, "Seleziona");
+                DropDownCity.DataSource = citys;
+                DropDownCity.DataBind();
+            }
+        }
+        
+        protected void LogIn_Click(object sender, EventArgs e)
+        {
+            CreateAndLoginUser();
+        }
+
+        private void CreateAndLoginUser()
+        {
+            if (!IsValid)
+            {
+                return;
+            }
+            var db = new UserOperationsContext();
+            var manager = Context.GetOwinContext().GetUserManager<ApplicationUserManager>();
+            var user = new ApplicationUser()
+            {
+                UserName = TextUserName.Text,
+                FirstName = TextFirstName.Text,
+                LastName = TextLastName.Text,
+                Address = TextAddress.Text,
+                Region = DropDownRegion.SelectedValue,
+                Province = DropDownProvince.SelectedValue,
+                City = DropDownCity.SelectedValue,
+                CellPhone = TextCellPhone.Text,
+                CF = TextCF.Text,
+                NeedsApproval = checkAssociated.Checked,
+                Email = TextEmail.Text 
+            };
+            var passwordValidator = new PasswordValidator();
+            passwordValidator.RequiredLength = 6;
+            manager.PasswordValidator = passwordValidator;
+            IdentityResult result = manager.Create(user);
+            if (result.Succeeded)
+            {
+                var loginInfo = Context.GetOwinContext().Authentication.GetExternalLoginInfo();
+                if (loginInfo == null)
+                {
+                    RedirectOnFail();
+                    return;
+                }
+                result = manager.AddLogin(user.Id, loginInfo.Login);
+                if (result.Succeeded)
+                {
+                    db.UsersData.Add(new UserData
+                    {
+                        UserName = user.UserName,
+                        Email = user.Email,
+                        FullName = user.FirstName + " " + user.LastName
+                    });
+                    db.SaveChanges();
+                    IdentityHelper.SignIn(manager, user, isPersistent: false);
+
+                    // For more information on how to enable account confirmation and password reset please visit http://go.microsoft.com/fwlink/?LinkID=320771
+                    // var code = manager.GenerateEmailConfirmationToken(user.Id);
+                    // Send this link via email: IdentityHelper.GetUserConfirmationRedirectUrl(code, user.Id)
+
+                    IdentityHelper.RedirectToReturnUrl(Request.QueryString["ReturnUrl"], Response);
+                    return;
+                }
+            }
+            AddErrors(result);
+        }
+
+        private void AddErrors(IdentityResult result) 
+        {
+            foreach (var error in result.Errors) 
+            {
+                ModelState.AddModelError("", error);
+            }
+        }
+    }
+}
