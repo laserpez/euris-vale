a55697509fac915fa8f407cd333a9e5ea64ea61a
Domenico Moreschini <domenico.moreschini@gmail.com>
2014-07-02 11:31:01 +0200

    Fix interfaccia grafica

---
+++ VALE/VALE/DownloadFile.ashx.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using VALE.Models;
+
+namespace VALE
+{
+    /// <summary>
+    /// Summary description for DownloadFile
+    /// </summary>
+    public class DownloadFile : IHttpHandler
+    {
+
+        public void ProcessRequest(HttpContext context)
+        {
+            HttpRequest request = HttpContext.Current.Request;
+            int fileId = Convert.ToInt16(request.QueryString["fileId"]);
+            using (var _db = new UserOperationsContext()) 
+            {
+                var file = _db.AttachedFiles.FirstOrDefault(f => f.AttachedFileID == fileId);
+                if (file != null) 
+                {
+                    HttpResponse response = HttpContext.Current.Response;
+                    response.ClearContent();
+                    response.Clear();
+                    response.ContentType = "application/octet-stream";
+                    response.AddHeader("Content-Disposition", "attachment; filename=" + file.FileName + ";");
+                    response.BinaryWrite(file.FileData);
+                    response.Flush();
+                    response.End();
+                }
+            } 
+        }
+
+        public bool IsReusable
+        {
+            get
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ VALE/VALE/Logic/MailHelper.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Web;
+
+namespace VALE.Logic
+{
+    public static class MailHelper
+    {
+
+
+        public static void SendMail(string email, string message, string subject)
+        {
+            var emailMessage = new MailMessage();
+            emailMessage.From = new MailAddress("vale.donotreply@gmail.com");
+            emailMessage.To.Add(new MailAddress(email));
+            emailMessage.Subject = subject;
+            emailMessage.Body = message;
+
+            var smtpClient = new SmtpClient();
+            smtpClient.EnableSsl = true;
+            
+            smtpClient.Send(emailMessage);
+        }
+    }
+}
