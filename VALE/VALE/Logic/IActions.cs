208a10114ae8aeae18c735c24f9e8be8ee527a88
roberto montinaro <roberto.montinaro@gmail.com>
2014-07-02 12:12:40 +0200

    Migliorata la gestione delle interfacce.

---
+++ VALE/VALE/Logic/IActions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.UI.WebControls;
+using VALE.Models;
+
+namespace VALE.Logic
+{
+    public interface IActions
+    {
+        bool AddOrRemoveUserData(int dataId, string username);
+
+        bool IsUserRelated(int dataId, string username);
+        bool IsGroupRelated(int dataId, int groupId);
+        
+        IQueryable<UserData> GetRelatedUsers(int _dataId);
+        IQueryable<Group> GetRelatedGroups(int _dataId);
+    }
+}
---
+++ VALE/VALE/Logic/ProjectActions.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Web;
+using System.Web.UI.WebControls;
+using VALE.Models;
+
+namespace VALE.Logic
+{
+    [Serializable]
+    public class ProjectActions : IActions
+    {
+        //UserOperationsContext _db = new UserOperationsContext();
+
+        public List<T> GetSortedData<T>(string sortExpression, SortDirection direction, List<T> data)
+        {
+            var result = data.Cast<Project>();
+
+            var param = Expression.Parameter(typeof(Project), sortExpression);
+            var sortBy = Expression.Lambda<Func<Project, object>>(Expression.Convert(Expression.Property(param, sortExpression), typeof(object)), param);
+
+            if (direction == SortDirection.Descending)
+                result = result.AsQueryable<Project>().OrderByDescending(sortBy).ToList();
+            else
+                result = result.AsQueryable<Project>().OrderBy(sortBy).ToList();
+
+            return (List<T>)result;
+        }
+
+        public List<T> GetFilteredData<T>(Dictionary<string, string> filters, List<T> data)
+        {
+            var result = data.Cast<Project>();
+            foreach (var filter in filters)
+            {
+                switch (filter.Key)
+                {
+                    case "Name":
+                        result = result.Where(p => p.ProjectName.ToLower().Contains(filter.Value.ToLower())).ToList();
+                        break;
+                    case "Description":
+                        result = result.Where(p => p.Description.ToLower().Contains(filter.Value.ToLower())).ToList();
+                        break;
+                    case "CreationDate":
+                        result = result.Where(p => p.CreationDate.ToShortDateString() == filter.Value).ToList();
+                        break;
+                    case "LastModifiedDate":
+                        result = result.Where(p => p.LastModified.ToShortDateString() == filter.Value).ToList();
+                        break;
+                    case "ProjectStatus":
+                        result = result.Where(p => p.Status.ToUpper() == filter.Value.ToUpper()).ToList();
+                        break;
+                    default:
+                        break;
+                }
+            }
+            return (List<T>)result;
+        }
+
+        public bool IsUserRelated(int projectId, string userName)
+        {
+            var db = new UserOperationsContext();
+            var project = db.Projects.First(p => p.ProjectId == projectId);
+            return project.InvolvedUsers.Select(u => u.UserName).Contains(userName);
+        }
+
+        public bool AddOrRemoveUserData<T>(T data, UserData user)
+        {
+            var project = data as Project;
+            if (IsUserRelated(project.ProjectId, user.UserName))
+            {
+                project.InvolvedUsers.Remove(user);
+                user.AttendingProjects.Remove(project);
+                return false;
+            }
+            else
+            {
+                project.InvolvedUsers.Add(user);
+                user.AttendingProjects.Add(project);
+                return true;
+            }
+        }
+
+        public bool RemoveAttachment(int attachmentId)
+        {
+            try
+            {
+                var db = new UserOperationsContext();
+                var anAttachment = db.AttachedFiles.FirstOrDefault(at => at.AttachedFileID == attachmentId);
+                db.AttachedFiles.Remove(anAttachment);
+
+                db.SaveChanges();
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        public bool AddAttachment(int dataId, AttachedFile file)
+        {
+            try
+            {
+                var db = new UserOperationsContext();
+                var project = db.Projects.First(p => p.ProjectId == dataId);
+                project.AttachedFiles.Add(file);
+                db.SaveChanges();
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        public List<AttachedFile> GetAttachments(int dataId)
+        {
+            var db = new UserOperationsContext();
+            var project = db.Projects.First(p => p.ProjectId == dataId);
+            return project.AttachedFiles;
+        }
+
+        public bool RemoveAllAttachments(int dataId)
+        {
+            try
+            {
+                var db = new UserOperationsContext();
+                var project = db.Projects.First(p => p.ProjectId == dataId);
+                db.AttachedFiles.RemoveRange(project.AttachedFiles);
+                db.SaveChanges();
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        public IQueryable<UserData> GetRelatedUsers(int dataId)
+        {
+            var db = new UserOperationsContext();
+            var project = db.Projects.First(p => p.ProjectId == dataId);
+            return project.InvolvedUsers.AsQueryable(); 
+        }
+
+
+        public bool AddOrRemoveUserData(int dataId, string username)
+        {
+            var db = new UserOperationsContext();
+            var aProject = db.Projects.First(p => p.ProjectId == dataId);
+            var user = db.UsersData.FirstOrDefault(u => u.UserName == username);
+            bool added = false;
+            if (!IsUserRelated(aProject.ProjectId, username))
+            {
+                aProject.InvolvedUsers.Add(user);
+                added = true;
+            }
+            else
+                aProject.InvolvedUsers.Remove(user);
+            db.SaveChanges();
+            return added;
+        }
+
+        public IQueryable<Group> GetRelatedGroups(int dataId)
+        {
+            var db = new UserOperationsContext();
+            var result = new List<Group>();
+            foreach(var group in db.Groups)
+            {
+                if (IsGroupRelated(dataId, group.GroupId))
+                    result.Add(group);
+            }
+            return result.AsQueryable();
+        }
+
+        public bool IsGroupRelated(int dataId, int groupId)
+        {
+            var db = new UserOperationsContext();
+            var group = db.Groups.First(g => g.GroupId == groupId);
+            var usersRelated = GetRelatedUsers(dataId);
+            return group.Users.Join(usersRelated, g => g.UserName, u => u.UserName, (g, u) => g.UserName + " " + u.UserName).Count() == group.Users.Count;
+        }
+    }
+}
