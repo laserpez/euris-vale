58ce3a3aec0efcfd96991d87fdcf555fc705a442
Federico Liuzzi <liuzzi.federico@gmail.com>
2014-07-02 12:41:46 +0200

    test di prova.

---
+++ VALE/VALE/MyVale/Articles.aspx.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using VALE.Models;
+
+namespace VALE.MyVale
+{
+    public partial class Articles : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        public IQueryable<BlogArticle> GetAllArticles()
+        {
+            var db = new UserOperationsContext();
+            return db.BlogArticles;
+        }
+
+        protected void grdAllArticles_RowCommand(object sender, GridViewCommandEventArgs e)
+        {
+
+
+            if (e.CommandName == "ViewArticle")
+            {
+                int index = Convert.ToInt32(e.CommandArgument);
+                int articleId = Convert.ToInt32(grdAllArticles.DataKeys[index].Value);
+                Response.Redirect("/MyVale/ViewArticle?articleId=" + articleId);
+            }
+        }
+    }
+}
