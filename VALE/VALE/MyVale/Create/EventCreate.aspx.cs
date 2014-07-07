7b26d866e179b60e404770f35f846be809488e42
Mekni Oussama <ing.oussama.mekni@gmail.com>
2014-07-07 13:03:17 +0200

    Commit (Oussama) : Risoluzione di piccoli Bug

---
+++ VALE/VALE/MyVale/Create/EventCreate.aspx.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Microsoft.AspNet.Identity;
+using VALE.Models;
+using System.IO;
+using VALE.Logic;
+
+namespace VALE.MyVale
+{
+    public partial class EventCreate : System.Web.UI.Page
+    {
+        private string _currentUser;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            _currentUser = User.Identity.GetUserName();
+            if (!IsPostBack)
+            {
+                if (Request.QueryString["ProjectId"] != null)
+                    Session["callingProjectId"] = Request.QueryString["ProjectId"];
+                else
+                    Session["callingProjectId"] = null;
+                calendarFrom.StartDate = DateTime.Now;
+            }
+
+            if (Session["callingProjectId"] != null)
+            {
+                var db = new UserOperationsContext();
+                var projectId = Convert.ToInt32(Session["callingProjectId"].ToString());
+                var projectName = db.Projects.First(p => p.ProjectId == projectId).ProjectName;
+                SelectProject.DisableControl(projectName);
+            }
+        }
+
+        protected void btnAddUsers_Click(object sender, EventArgs e)
+        {
+            var db = new UserOperationsContext();
+            var user = db.UserDatas.FirstOrDefault(u => u.UserName == _currentUser);
+            var registeredUsers = new List<UserData>() { user };
+            
+            var newEvent = new Event
+            {
+                Name = txtName.Text,
+                Description = txtDescription.Text,
+                Public = chkPublic.Checked,
+                EventDate = Convert.ToDateTime(txtStartDate.Text),
+                OrganizerUserName = _currentUser,
+                RegisteredUsers = registeredUsers,
+                RelatedProject = db.Projects.FirstOrDefault(p => p.ProjectName == SelectProject.ProjectNameTextBox.Text)
+            };
+            var eventActions = new EventActions();
+            
+            eventActions.SaveData(newEvent, db);
+            
+            var redirectURL = "";
+            if (Session["callingProjectId"] != null)
+                redirectURL = "/MyVale/ProjectDetails?projectId=" + Session["callingProjectId"].ToString();
+            else if (Session["requestFrom"] != null)
+                redirectURL = Session["requestFrom"].ToString();
+            else
+                redirectURL = "/MyVale/Events";
+
+            Response.Redirect("/MyVale/UserSelector.aspx?dataId=" + newEvent.EventId + "&dataType=event&returnUrl=" + redirectURL);
+        }
+    }
+}
---
+++ VALE/VALE/MyVale/Create/SelectProject.ascx.designer.cs
@@ -0,0 +1,87 @@
+﻿//------------------------------------------------------------------------------
+// <generato automaticamente>
+//     Codice generato da uno strumento.
+//
+//     Le modifiche a questo file possono causare un comportamento non corretto e verranno perse se
+//     il codice viene rigenerato. 
+// </generato automaticamente>
+//------------------------------------------------------------------------------
+
+namespace VALE.MyVale.Create {
+    
+    
+    public partial class SelectProject {
+        
+        /// <summary>
+        /// Controllo SearchProjectPanel.
+        /// </summary>
+        /// <remarks>
+        /// Campo generato automaticamente.
+        /// Per la modifica, spostare la dichiarazione di campo dal file di progettazione al file code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.UpdatePanel SearchProjectPanel;
+        
+        /// <summary>
+        /// Controllo txtProjectName.
+        /// </summary>
+        /// <remarks>
+        /// Campo generato automaticamente.
+        /// Per la modifica, spostare la dichiarazione di campo dal file di progettazione al file code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtProjectName;
+        
+        /// <summary>
+        /// Controllo btnShowPopup.
+        /// </summary>
+        /// <remarks>
+        /// Campo generato automaticamente.
+        /// Per la modifica, spostare la dichiarazione di campo dal file di progettazione al file code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnShowPopup;
+        
+        /// <summary>
+        /// Controllo txtProjectAutoCompleter.
+        /// </summary>
+        /// <remarks>
+        /// Campo generato automaticamente.
+        /// Per la modifica, spostare la dichiarazione di campo dal file di progettazione al file code-behind.
+        /// </remarks>
+        protected global::AjaxControlToolkit.AutoCompleteExtender txtProjectAutoCompleter;
+        
+        /// <summary>
+        /// Controllo ModalPopup.
+        /// </summary>
+        /// <remarks>
+        /// Campo generato automaticamente.
+        /// Per la modifica, spostare la dichiarazione di campo dal file di progettazione al file code-behind.
+        /// </remarks>
+        protected global::AjaxControlToolkit.ModalPopupExtender ModalPopup;
+        
+        /// <summary>
+        /// Controllo lnkDummy.
+        /// </summary>
+        /// <remarks>
+        /// Campo generato automaticamente.
+        /// Per la modifica, spostare la dichiarazione di campo dal file di progettazione al file code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.LinkButton lnkDummy;
+        
+        /// <summary>
+        /// Controllo TitleMpdalView.
+        /// </summary>
+        /// <remarks>
+        /// Campo generato automaticamente.
+        /// Per la modifica, spostare la dichiarazione di campo dal file di progettazione al file code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label TitleMpdalView;
+        
+        /// <summary>
+        /// Controllo OpenedProjectList.
+        /// </summary>
+        /// <remarks>
+        /// Campo generato automaticamente.
+        /// Per la modifica, spostare la dichiarazione di campo dal file di progettazione al file code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.GridView OpenedProjectList;
+    }
+}
