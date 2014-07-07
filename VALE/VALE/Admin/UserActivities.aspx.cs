5bcb539bcbdbb8c7d6462d67d39de396b34be346
roberto montinaro <roberto.montinaro@gmail.com>
2014-07-07 17:35:12 +0200

    Corretto pulsante gridView

---
+++ VALE/VALE/Admin/UserActivities.aspx.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.ModelBinding;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using VALE.Models;
+
+namespace VALE.Admin
+{
+    public partial class UserActivities : System.Web.UI.Page
+    {
+        private int _activityId;
+        private string _userEmail;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Request.QueryString.HasKeys())
+            {
+                _activityId = Convert.ToInt32(Request.QueryString.GetValues("activityId").First());
+                _userEmail = Request.QueryString.GetValues("userId").First();
+            }
+        }
+
+        public IQueryable<ActivityReport> GetUserActivities([QueryString("activityId")] int? activityId, [QueryString("userId")] string userEmail)
+        {
+            var db = new UserOperationsContext();
+            if (activityId.HasValue && !String.IsNullOrEmpty(userEmail))
+            {
+                return db.Reports.Where(r => r.Worker.Email == userEmail && r.ActivityId == activityId);
+            }
+            else
+                return null;
+        }
+
+        public string GetWorkerName()
+        {
+            var db = new UserOperationsContext();
+            return db.UserDatas.First(u => u.Email == _userEmail).FullName;
+        }
+
+        public string GetWorkedActivity()
+        {
+            var db = new UserOperationsContext();
+            return db.Activities.First(a => a.ActivityId == _activityId).ActivityName;
+        }
+
+        protected void grdReports_DataBound(object sender, EventArgs e)
+        {
+            HeaderName.Text = String.Format("Report per l'utente {0} sull'attività {1}", GetWorkerName(), GetWorkedActivity());
+        }
+
+
+    }
+}
