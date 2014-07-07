6a0476e310607576a45c142a9d6a0638b56d21f3
Federico Liuzzi <liuzzi.federico@gmail.com>
2014-07-07 15:12:23 +0200

    completata pagina documentsvale.

--- VALE/VALE/MyVale/BOD/VALEDocuments.aspx.cs
+++ VALE/VALE/MyVale/BOD/VALEDocuments.aspx.cs
@@ -1,17 +1,86 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using VALE.Logic;
+using VALE.Models;
 
 namespace VALE.MyVale.BOD
 {
-    public partial class VALEDocuments : System.Web.UI.Page
+    public partial class VALEDocuments : Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (HttpContext.Current.User.IsInRole("Amministratore") || HttpContext.Current.User.IsInRole("Membro del consiglio"))
+            {
+                FooterDocuments.Visible = true;
+            }
+            else
+            {
+                FooterDocuments.Visible = false;
+            }
+        }
 
+        public int DataId
+        {
+            get { return (int)ViewState["datavaleId"]; }
+            set { ViewState["datavaleId"] = value; }
+        }
+
+        public bool AllowUpload { get; set; }
+
+        public IQueryable<ValeFile> DocumentsGridView_GetData()
+        {
+            var db = new UserOperationsContext();
+            return db.VALEFiles;
+        }
+
+        protected void AddFileNameButton_Click(object sender, EventArgs e)
+        {
+            var db = new UserOperationsContext();
+            var attachedFile = new ValeFile();
+            if (FileUpload.HasFile)
+            {
+                attachedFile.FileName = FileUpload.PostedFile.FileName;
+                attachedFile.FileDescription = txtFileDescription.Text;
+                attachedFile.FileExtension = Path.GetExtension(FileUpload.PostedFile.FileName);
+                attachedFile.FileData = FileUpload.FileBytes;
+                txtFileDescription.Text = "";
+                db.VALEFiles.Add(attachedFile);
+                db.SaveChanges();
+                DocumentsGridView.DataBind();
+            }
+        }
+
+        protected void grdFilesUploaded_RowCommand(object sender, GridViewCommandEventArgs e)
+        {
+            var db = new UserOperationsContext();
+            int id = Convert.ToInt32(e.CommandArgument);
+            switch (e.CommandName)
+            {
+                case "DOWNLOAD":
+                default:
+                    Response.Redirect("/DownloadFile.ashx?fileId=" + id+"&page=VALEDocuments");
+                    break;
+                case "Cancella":
+                    var elem = db.VALEFiles.Where(o => o.ValeFileID == id).FirstOrDefault();
+                    db.VALEFiles.Remove(elem);
+                    db.SaveChanges();
+                    DocumentsGridView.DataBind();
+                    break;
+            }
+        }
+
+        public bool AllowDelete(int attachedFileId)
+        {
+            var db = new UserOperationsContext();
+            var attachedFile = db.VALEFiles.First(a => a.ValeFileID == attachedFileId);
+            if (HttpContext.Current.User.IsInRole("Amministratore") || HttpContext.Current.User.IsInRole("Membro del consiglio"))
+                return true;
+            return false;
         }
     }
 }
