85c9fd8173b311b2bdd10b383f1fa5e623d365e7
Elena Bracchi <erika.dreven@gmail.com>
2014-07-07 14:52:11 +0200

    Pagina profilo e gestione profilo. Manca solo la gestione della password.

--- VALE/VALE/Account/Profile.aspx.cs
+++ VALE/VALE/Account/Profile.aspx.cs
@@ -18,28 +18,36 @@ namespace VALE
             _currentUser = HttpContext.Current.User.Identity.Name;
             HeaderName.Text = "Profilo di " + _currentUser;
 
-            //SetImage();
+            SetImage();
+            SetCV();
         }
 
-        //private void SetImage()
-        //{
-        //    var db = new ApplicationDbContext();
-        //    FileStream stream = new FileStream("test.png", FileMode.Open, FileAccess.Read);
-        //    photoProfile. = Image.FromStream(stream);
-        //    stream.Close();
-        //}
+        private void SetCV()
+        {
+            var buttonAttachment = (LinkButton)PersonalDataFormView.FindControl("CVAttachment");
 
-        //public ActionResult showImg(int id)
-        //{
-        //    var db = new ApplicationDbContext();
+            var db = new ApplicationDbContext();
+            var user = db.Users.Where(u => u.UserName == _currentUser).FirstOrDefault();
+            if (user.Document== null)
+            {
+                buttonAttachment.Text = "Nessun CV attualmente caricato";
+                buttonAttachment.Enabled = false;
+            }
+            else
+            {
+                buttonAttachment.Text = user.CVName;
+            }
+        }
 
-        //    //var imageData = from m in db.Users
-        //    //                select Image.FromStream(new MemoryStream(m.PhotoProfile.ToArray()));
-
-        //    var stream = (from m in db.Users select m.PhotoProfile).FirstOrDefault();
-
-        //    return Image(stream, "image/jpeg");
-        //}
+        private void SetImage()
+        {
+            var db = new ApplicationDbContext();
+            var imageBytes = db.Users.Where(u => u.UserName == _currentUser).Select(us => us.PhotoProfile).FirstOrDefault();
+            if (imageBytes != null)
+                picProfile.Src = "data:image/png;base64," + Convert.ToBase64String(imageBytes);
+            else
+                picProfile.Src = "~/Images/User.jpg";
+        }
 
         public ApplicationUser GetUserData()
         {
@@ -58,6 +66,23 @@ namespace VALE
         protected void EditProfile_Click(object sender, EventArgs e)
         {
             Response.Redirect("~/Account/MenageProfile?currentUser=" + _currentUser);
+        }
+
+        protected void CVAttachment_Click(object sender, EventArgs e)
+        {
+            var db = new ApplicationDbContext();
+            var user = db.Users.Where(u => u.UserName == _currentUser).FirstOrDefault();
+            if (user.Document != null)
+            {
+                HttpResponse response = HttpContext.Current.Response;
+                response.ClearContent();
+                response.Clear();
+                response.ContentType = "Application/octet-stream";
+                response.AddHeader("Content-Disposition", "attachment; filename=" + user.CVName + ";");
+                response.BinaryWrite(user.Document);
+                response.Flush();
+                response.End();
+            }
         }
     }
 }
