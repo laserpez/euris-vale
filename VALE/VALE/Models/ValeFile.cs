b914af16ca42adb3ccd5bdf25f5c2a2c3c64e1f2
Domenico Moreschini <domenico.moreschini@gmail.com>
2014-07-07 15:19:57 +0200

    Fix

---
+++ VALE/VALE/Models/ValeFile.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+
+namespace VALE.Models
+{
+    public class ValeFile
+    {
+        public int ValeFileID { get; set; }
+        public string FileName { get; set; }
+        public string FileDescription { get; set; }
+        public string FileExtension { get; set; }
+        public byte[] FileData { set; get; }
+    }
+}
---
+++ VALE/VALE/MyVale/Create/ProjectCreate.aspx.designer.cs
@@ -0,0 +1,87 @@
+﻿//------------------------------------------------------------------------------
+// <generato automaticamente>
+//     Codice generato da uno strumento.
+//
+//     Le modifiche a questo file possono causare un comportamento non corretto e verranno perse se
+//     il codice viene rigenerato. 
+// </generato automaticamente>
+//------------------------------------------------------------------------------
+
+namespace VALE.MyVale {
+    
+    
+    public partial class ProjectCreate {
+        
+        /// <summary>
+        /// Controllo HeaderName.
+        /// </summary>
+        /// <remarks>
+        /// Campo generato automaticamente.
+        /// Per la modifica, spostare la dichiarazione di campo dal file di progettazione al file code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label HeaderName;
+        
+        /// <summary>
+        /// Controllo txtName.
+        /// </summary>
+        /// <remarks>
+        /// Campo generato automaticamente.
+        /// Per la modifica, spostare la dichiarazione di campo dal file di progettazione al file code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtName;
+        
+        /// <summary>
+        /// Controllo txtDescription.
+        /// </summary>
+        /// <remarks>
+        /// Campo generato automaticamente.
+        /// Per la modifica, spostare la dichiarazione di campo dal file di progettazione al file code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtDescription;
+        
+        /// <summary>
+        /// Controllo txtStartDate.
+        /// </summary>
+        /// <remarks>
+        /// Campo generato automaticamente.
+        /// Per la modifica, spostare la dichiarazione di campo dal file di progettazione al file code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtStartDate;
+        
+        /// <summary>
+        /// Controllo calendarFrom.
+        /// </summary>
+        /// <remarks>
+        /// Campo generato automaticamente.
+        /// Per la modifica, spostare la dichiarazione di campo dal file di progettazione al file code-behind.
+        /// </remarks>
+        protected global::AjaxControlToolkit.CalendarExtender calendarFrom;
+        
+        /// <summary>
+        /// Controllo chkPublic.
+        /// </summary>
+        /// <remarks>
+        /// Campo generato automaticamente.
+        /// Per la modifica, spostare la dichiarazione di campo dal file di progettazione al file code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.CheckBox chkPublic;
+        
+        /// <summary>
+        /// Controllo SelectProject.
+        /// </summary>
+        /// <remarks>
+        /// Campo generato automaticamente.
+        /// Per la modifica, spostare la dichiarazione di campo dal file di progettazione al file code-behind.
+        /// </remarks>
+        protected global::VALE.MyVale.Create.SelectProject SelectProject;
+        
+        /// <summary>
+        /// Controllo btnSaveActivity.
+        /// </summary>
+        /// <remarks>
+        /// Campo generato automaticamente.
+        /// Per la modifica, spostare la dichiarazione di campo dal file di progettazione al file code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnSaveActivity;
+    }
+}
