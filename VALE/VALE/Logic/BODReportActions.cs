ba09d5f7f0971c60550ff7331aa801cdc9c12c7a
Federico Liuzzi <liuzzi.federico@gmail.com>
2014-07-08 17:06:24 +0200

    ramo test.

---
+++ VALE/VALE/Logic/BODReportActions.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using VALE.Models;
+
+namespace VALE.Logic
+{
+    [Serializable]
+    public class BODReportActions : IFileActions
+    {
+        public bool AddAttachment(int dataId, Models.AttachedFile file)
+        {
+            try
+            {
+                var db = new UserOperationsContext();
+                var report = db.BODReports.First(r => r.BODReportId == dataId);
+                report.AttachedFiles.Add(file);
+                db.SaveChanges();
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        public bool RemoveAttachment(int attachmentId)
+        {
+            try
+            {
+                var db = new UserOperationsContext();
+                var anAttachment = db.AttachedFiles.FirstOrDefault(at => at.AttachedFileID == attachmentId);
+                db.AttachedFiles.Remove(anAttachment);
+
+                db.SaveChanges();
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        public List<Models.AttachedFile> GetAttachments(int dataId)
+        {
+            var db = new UserOperationsContext();
+            var report = db.BODReports.First(r => r.BODReportId == dataId);
+            return report.AttachedFiles;
+        }
+
+
+        public bool RemoveAllAttachments(int dataId)
+        {
+            try
+            {
+                var db = new UserOperationsContext();
+                var BODReport = db.BODReports.First(b => b.BODReportId == dataId);
+                db.AttachedFiles.RemoveRange(BODReport.AttachedFiles);
+                db.SaveChanges();
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+    }
+}
