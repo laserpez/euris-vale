d939c19f540610273ef1407404c74d6735a00e22
Elena Bracchi <erika.dreven@gmail.com>
2014-07-09 11:11:11 +0200

    Cambiato salvataggio in project.

---
+++ VALE/VALE/Models/Group.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace VALE.Models
+{
+    public class Group
+    {
+        public int GroupId { get; set; }
+        public string GroupName { get; set; }
+        public string Description { get; set; }
+        public DateTime CreationDate { get; set; }
+        public virtual List<UserData> Users { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            var group = (Group)obj;
+            return this.GroupId == group.GroupId;
+        }
+
+        public override int GetHashCode()
+        {
+            return GroupId.GetHashCode();
+        }
+    }
+}
