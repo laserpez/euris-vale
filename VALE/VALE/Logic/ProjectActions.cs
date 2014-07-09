5725e63d2c4ccffc413e70230e1edeac74c0e608
Federico Liuzzi <liuzzi.federico@gmail.com>
2014-07-09 10:44:47 +0200

    inizio modifiche nuove

---
+++ VALE/VALE/Logic/ProjectActions.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Web;
+using System.Web.UI.WebControls;
+using VALE.Models;
+
+namespace VALE.Logic
+{
+    [Serializable]
+    public class ProjectActions : IActions, IFileActions
+    {
+        public ILogger logger { get; set; }
+
+        public ProjectActions()
+        {
+            logger = LogFactory.GetCurrentLogger();
+
+        }
+
+        public List<T> GetSortedData<T>(string sortExpression, SortDirection direction, List<T> data)
+        {
+            var result = data.Cast<Project>();
+
+            var param = Expression.Parameter(typeof(Project), sortExpression);
+            var sortBy = Expression.Lambda<Func<Project, object>>(Expression.Convert(Expression.Property(param, sortExpression), typeof(object)), param);
+
+            if (direction == SortDirection.Descending)
+                result = result.AsQueryable<Project>().OrderByDescending(sortBy).ToList();
+            else
+                result = result.AsQueryable<Project>().OrderBy(sortBy).ToList();
+
+            return (List<T>)result;
+        }
+
+        public List<T> GetFilteredData<T>(Dictionary<string, string> filters, List<T> data)
+        {
+            var result = data.Cast<Project>();
+            foreach (var filter in filters)
+            {
+                switch (filter.Key)
+                {
+                    case "Name":
+                        result = result.Where(p => p.ProjectName.ToLower().Contains(filter.Value.ToLower())).ToList();
+                        break;
+                    case "Description":
+                        result = result.Where(p => p.Description.ToLower().Contains(filter.Value.ToLower())).ToList();
+                        break;
+                    case "CreationDate":
+                        result = result.Where(p => p.CreationDate.ToShortDateString() == filter.Value).ToList();
+                        break;
+                    case "LastModifiedDate":
+                        result = result.Where(p => p.LastModified.ToShortDateString() == filter.Value).ToList();
+                        break;
+                    case "ProjectStatus":
+                        result = result.Where(p => p.Status.ToUpper() == filter.Value.ToUpper()).ToList();
+                        break;
+                    default:
+                        break;
+                }
+            }
+            return (List<T>)result;
+        }
+
+        public bool IsUserRelated(int projectId, string userName)
+        {
+            var db = new UserOperationsContext();
+            var project = db.Projects.First(p => p.ProjectId == projectId);
+            return project.InvolvedUsers.Select(u => u.UserName).Contains(userName);
+        }
+
+        public bool RemoveAttachment(int attachmentId)
+        {
+            try
+            {
+                var db = new UserOperationsContext();
+                var anAttachment = db.AttachedFiles.FirstOrDefault(at => at.AttachedFileID == attachmentId);
+                db.AttachedFiles.Remove(anAttachment);
+                anAttachment.RelatedProject.LastModified = DateTime.Now;
+                db.SaveChanges();
+                logger.Write(new LogEntry() { DataId = anAttachment.RelatedProject.ProjectId, Username = HttpContext.Current.User.Identity.Name, DataAction = "Rimosso documento da \"" + anAttachment.RelatedProject.ProjectName + "\"", DataType = "Progetto", Date = DateTime.Now, Description = "Nome documento: \"" + anAttachment.FileName + "\"" });
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        public bool AddAttachment(int dataId, AttachedFile file)
+        {
+            try
+            {
+                var db = new UserOperationsContext();
+                var project = db.Projects.First(p => p.ProjectId == dataId);
+                project.AttachedFiles.Add(file);
+                project.LastModified = DateTime.Now;
+                db.SaveChanges();
+                logger.Write(new LogEntry() { DataId = project.ProjectId, Username = HttpContext.Current.User.Identity.Name, DataAction = "Aggiunto documento a \"" + project.ProjectName + "\"", DataType = "Progetto", Date = DateTime.Now, Description = "Nome documento: \"" + file.FileName + "\"" });
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        public List<AttachedFile> GetAttachments(int dataId)
+        {
+            var db = new UserOperationsContext();
+            var project = db.Projects.First(p => p.ProjectId == dataId);
+            return project.AttachedFiles;
+        }
+
+        public bool RemoveAllAttachments(int dataId)
+        {
+            try
+            {
+                var db = new UserOperationsContext();
+                var project = db.Projects.First(p => p.ProjectId == dataId);
+                db.AttachedFiles.RemoveRange(project.AttachedFiles);
+                db.SaveChanges();
+                logger.Write(new LogEntry() { DataId = dataId, Username = HttpContext.Current.User.Identity.Name, DataAction = "Rimossi tutti i documenti", DataType = "Progetto", Date = DateTime.Now, Description = "Rimossi tutti i documenti da \"" + project.ProjectName + "\"" });
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        public IQueryable<UserData> GetRelatedUsers(int dataId)
+        {
+            var db = new UserOperationsContext();
+            var project = db.Projects.First(p => p.ProjectId == dataId);
+            return project.InvolvedUsers.AsQueryable(); 
+        }
+
+
+        public bool AddOrRemoveUserData(int dataId, string username)
+        {
+            var db = new UserOperationsContext();
+            var aProject = db.Projects.First(p => p.ProjectId == dataId);
+            var user = db.UserDatas.FirstOrDefault(u => u.UserName == username);
+            bool added = false;
+            if (!IsUserRelated(aProject.ProjectId, username))
+            {
+                aProject.InvolvedUsers.Add(user);
+                added = true;
+            }
+            else
+                aProject.InvolvedUsers.Remove(user);
+            db.SaveChanges();
+
+            logger.Write(new LogEntry() { DataId = dataId, Username = user.UserName, DataAction = added ? "Aggiunto utente" : "Rimosso utente", DataType = "Progetto", Date = DateTime.Now, Description = user.UserName + (added ? " è stato invitato a \"" : " non collabora più a \"") + aProject.ProjectName + "\"" });
+            return added;
+        }
+
+        public IQueryable<Group> GetRelatedGroups(int dataId)
+        {
+            var db = new UserOperationsContext();
+            var result = new List<Group>();
+            foreach(var group in db.Groups)
+            {
+                if (IsGroupRelated(dataId, group.GroupId))
+                    result.Add(group);
+            }
+            return result.AsQueryable();
+        }
+
+        public bool IsGroupRelated(int dataId, int groupId)
+        {
+            var db = new UserOperationsContext();
+            var group = db.Groups.First(g => g.GroupId == groupId);
+            var usersRelated = GetRelatedUsers(dataId);
+            return group.Users.Join(usersRelated, g => g.UserName, u => u.UserName, (g, u) => g.UserName + " " + u.UserName).Count() == group.Users.Count;
+        }
+
+
+        public bool SaveData<T>(T data, UserOperationsContext db)
+        {
+            try
+            {
+                var project = data as Project;
+                db.Projects.Add(project);
+                db.SaveChanges();
+                logger.Write(new LogEntry() { DataId = project.ProjectId, Username = HttpContext.Current.User.Identity.Name, DataAction = "Creato nuovo progetto", DataType = "Progetto", Date = DateTime.Now, Description ="Nome progetto: \"" + project.ProjectName + "\"" });
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ VALE/VALE/MyVale/CreateIntervention.aspx.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Microsoft.AspNet.Identity;
+using System.IO;
+using VALE.Models;
+using System.Data.Entity.Validation;
+
+namespace VALE.MyVale
+{
+    public partial class CreateIntervention : System.Web.UI.Page
+    {
+        private string _currentUser;
+        private int _currentProjectId;
+        private string _temporaryPath;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            //_currentUser = User.Identity.GetUserName();
+            //_temporaryPath = "/MyVale/Documents/Temp/" + _currentUser + "/";
+            //if (Request.QueryString.HasKeys())
+            //    _currentProjectId = Convert.ToInt32(Request.QueryString.GetValues("projectId").First());
+            //if (!IsPostBack)
+            //{
+            //    if (!String.IsNullOrEmpty(_temporaryPath))
+            //    {
+            //        if (Directory.Exists(Server.MapPath(_temporaryPath)))
+            //            Directory.Delete(Server.MapPath(_temporaryPath), true);
+            //        Directory.CreateDirectory(Server.MapPath(_temporaryPath));
+            //    }
+
+            //    PopulateGridView();
+            //}
+        }
+
+        protected void btnSaveIntervention_Click(object sender, EventArgs e)
+        {
+            //var db = new UserOperationsContext();
+            //var intervention = new Intervention
+            //{
+            //    CreatorUserName = _currentUser,
+            //    ProjectId = _currentProjectId,
+            //    InterventionText = txtComment.Text,
+            //    Date = DateTime.Today
+            //};
+            //db.Interventions.Add(intervention);
+            //db.SaveChanges();
+            //intervention.DocumentsPath = "/MyVale/Documents/Interventions/" + intervention.InterventionId + "/";
+            //string serverPath = Server.MapPath(intervention.DocumentsPath);
+            //string tempPath = Server.MapPath(_temporaryPath);
+            //if (!Directory.Exists(Server.MapPath("/MyVale/Documents/Interventions/")))
+            //    Directory.CreateDirectory(Server.MapPath("/MyVale/Documents/Interventions/"));
+            //Directory.Move(tempPath, serverPath);
+            //db.SaveChanges();
+
+            //Response.Redirect("/MyVale/ProjectDetails?projectId=" + _currentProjectId);
+        }
+
+        protected void btnUploadFile_Click(object sender, EventArgs e)
+        {
+            //if (FileUploadControl.HasFiles)
+            //{
+            //    FileUploadControl.SaveAs(Server.MapPath(_temporaryPath + Path.GetFileName(FileUploadControl.PostedFile.FileName)));
+            //}
+            //PopulateGridView();
+        }
+
+        protected void grdFilesUploaded_RowCommand(object sender, GridViewCommandEventArgs e)
+        {
+            GridView grid = (GridView)sender;
+            int index = Convert.ToInt32(e.CommandArgument);
+            string fileToRemove = grid.Rows[index].Cells[1].Text;
+            File.Delete(Server.MapPath(_temporaryPath) + fileToRemove);
+            PopulateGridView();
+        }
+
+        private void PopulateGridView()
+        {
+            DirectoryInfo dirInfo = new DirectoryInfo(Server.MapPath(_temporaryPath));
+            if (dirInfo.Exists)
+                grdFilesUploaded.DataSource = dirInfo.GetFiles().Select(o => new { Filename = o.Name });
+            else
+                grdFilesUploaded.DataSource = null;
+            grdFilesUploaded.DataBind();
+        }
+    }
+}
