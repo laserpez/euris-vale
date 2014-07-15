b7dbe2a34e9814c2182abf055aceaeb104a7a0c4
Federico Liuzzi <liuzzi.federico@gmail.com>
2014-07-15 16:53:20 +0200

    sistemato site.master dopo merge ruoli

--- VALE/VALE/Site.Master.designer.cs
+++ VALE/VALE/Site.Master.designer.cs
@@ -58,6 +58,15 @@ namespace VALE {
         protected global::System.Web.UI.HtmlControls.HtmlGenericControl NotificationUsersRequest;
         
         /// <summary>
+        /// Span1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlGenericControl Span1;
+        
+        /// <summary>
         /// NotificationArticlesRequest control.
         /// </summary>
         /// <remarks>
