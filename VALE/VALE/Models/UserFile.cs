fdf55f14fccf64c5ee046be2c263fac404e206aa
Mekni Oussama <ing.oussama.mekni@gmail.com>
2014-09-16 23:14:56 +0200

    commit

---
+++ VALE/VALE/Models/UserFile.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace VALE.Models
+{
+    public class UserFile
+    {
+        public int UserFileID { get; set; }
+        public string UserName { get; set; }
+        public string FileName { get; set; }
+        public string FileDescription { get; set; }
+        public string FileExtension { get; set; }
+        public int Size { get; set; }
+        public bool IsPublic { get; set; }
+        public byte[] FileData { set; get; }
+    }
+}
